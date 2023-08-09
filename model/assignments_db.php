b3ca80f153a257cff43ac5a72c95b0c62bfebcb6
kendevtech <kendallsalazarsoto@gmail.com>
2023-08-09 11:41:05 -0600

    DB query

---
+++ model/assignments_db.php
@@ -0,0 +1,41 @@
+<?php
+
+function get_assignment_by_course($course_id)
+{
+    global $db;
+    if ($course_id) {
+        $query = 'SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN courses C ON A.courseID = C.ID WHERE A.courseID = :course_id ORDER BY A.ID';
+    } else {
+        $query = 'SELECT A.ID, A.Description, C.courseName FROM assignments A LEFT JOIN courses C ON A.courseID = C.ID ORDER BY C.courseID';
+    }
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':course_id', $course_id);
+    $statement->execute();
+    $assignments = $statement->fetchAll();
+    $statement->closeCursor();
+    return $assignments;
+}
+
+function delete_assignment($assignment_id)
+{
+    global $db;
+    $query = 'DELETE FROM assignments WHERE ID = :assign_id';
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':assign_id', $assignment_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function add_assignment($course_id, $description)
+{
+    global $db;
+    $query = 'INSERT INTO assignments (Description, courseID) VALUES (:descr, :courseID)';
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':descr', $description);
+    $statement->bindValue(':courseID', $course_id);
+    $statement->execute();
+    $statement->closeCursor();
+}
---
+++ model/course_db.php
@@ -0,0 +1,29 @@
+<?php
+
+
+function get_courses()
+{
+    global $db;
+    $query = 'SELECT * FROM courses ORDER BY courseID';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $courses = $statement->fetchAll();
+    $statement->closeCursor();
+    return $courses;
+}
+
+function get_course_name($course_id)
+{
+    if (!$course_id) {
+        return "All Courses";
+    }
+    global $db;
+    $query = 'SELECT * FROM courses WHERE courseID = :course_id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':course_id', $course_id);
+    $statement->execute();
+    $course = $statement->fetch();
+    $statement->closeCursor();
+    $course_name = $course['courseName'];
+    return $course_name;
+}
